269f8f34fb9a22727ecaf6468c9e1fcb11d74d81
Jeheon Choi <c92921@gmail.com>
2019-11-26 04:33:25 +0900

    Update Config.py
    EPISODES, MCTS_SIMS 등 줄임

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #### SELF PLAY
-EPISODES = 30
-MCTS_SIMS = 50
-MEMORY_SIZE = 30000
+EPISODES = 18
+MCTS_SIMS = 30
+MEMORY_SIZE = 18000
 TURNS_UNTIL_TAU0 = 10 # turn on which it starts playing deterministically
 CPUCT = 1
 EPSILON = 0.2
