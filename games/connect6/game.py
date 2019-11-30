3b7c1c0c105aba0524e96473a5d2af869f3d5b67
Jeheon Choi <c92921@gmail.com>
2019-12-01 02:48:31 +0900

    Change some parameters for Connect6

--- games/connect6/game.py
+++ games/connect6/game.py
@@ -1,8 +1,8 @@
 import numpy as np
 import logging
 
-ROW = 19
-COL = 19
+ROW = 9
+COL = 9
 INIT_BOARD = np.zeros(ROW * COL, dtype=np.int)
 INIT_CURRENT_PLAYER = -1
 INIT_BOARD[int(ROW*COL/2)] = -INIT_CURRENT_PLAYER	# 첫 수로 정중앙에 한 수를 놓음
