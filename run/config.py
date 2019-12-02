5d29646dfd18185b8e798a1fed279e353505ca1c
Jeheon Choi <c92921@gmail.com>
2019-12-02 22:20:21 +0900

    Add model 52 & memory140

--- run/config.py
+++ run/config.py
@@ -1,8 +1,8 @@
 #### SELF PLAY
-EPISODES = 30
+EPISODES = 25
 MCTS_SIMS = 50
 MEMORY_SIZE = 30000
-TURNS_UNTIL_TAU0 = 10 # turn on which it starts playing deterministically
+TURNS_UNTIL_TAU0 = 6 # turn on which it starts playing deterministically
 CPUCT = 1
 EPSILON = 0.2
 ALPHA = 0.8
