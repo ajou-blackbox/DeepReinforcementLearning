b8195436ecfff59c7ba62b38210d483c4bd37b64
Han Noeul <hypnec85@gmail.com>
2020-05-10 14:49:42 +0900

    Fix Integrator of agent.py
    Integrator의 사소한 버그 수정

--- agent.py
+++ agent.py
@@ -147,12 +147,12 @@ class Agent():
 		# Policy 가중치
 		frag_policy_weight = []
 		for i in range(64):
-			frag_policy_weight[i] = abs(value[i]) + 1
+			frag_policy_weight.append( abs(value[i]) + 1 )
 			frag_policy_weight[i] *= frag_allowed_count[i]
 
 		# 19*19에 합산
-		number_count = np.zeros(len(self.state_size), dtype=np.int)
-		total_logits = np.zeros(len(self.action_size), dtype=np.int)
+		number_count = np.zeros(self.state_size, dtype=np.int)
+		total_logits = np.zeros(self.state_size, dtype=np.int)
 		for n in range(64):
 			start_num = int(n/8)*19 + n%8
 			for i in range(12):
@@ -160,7 +160,7 @@ class Agent():
 					now_num = start_num + i*19 + j
 					total_logits[now_num] += logits[n][i*12 + j] * frag_policy_weight[n]
 					number_count[now_num] += 1
-		for i in range(len(self.action_size)):
+		for i in range(self.action_size):
 			total_logits[i] /= number_count[i]
 
 		# 둘 수 없는 곳 policy 낮게 만듬
