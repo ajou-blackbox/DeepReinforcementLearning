a158b41c332e3355bfe8885719c6a497811a65b2
Han Noeul <hypnec85@gmail.com>
2020-05-31 15:55:30 +0900

    modify mcts
    오타 수정

--- agent.py
+++ agent.py
@@ -241,6 +241,6 @@ class Agent():
 		# self.mcts.root = self.mcts.tree[state.id]
 		temp_mcts = mc.MCTS(self.mcts.tree[state.id], self.cpuct)
 		for idx, nodeid in enumerate(self.mcts.recentnodeid):
-			if nodeid in self.mcts.tree	
+			if nodeid in self.mcts.tree	:
 				temp_mcts.addNode(self.mcts.tree[nodeid])
 		self.mcts = temp_mcts
