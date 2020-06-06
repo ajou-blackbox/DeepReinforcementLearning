e4b04f457e0d8ca04feb91502c46b5b745e9c602
Han Noeul <hypnec85@gmail.com>
2020-06-06 15:22:58 +0900

    modify fileflag
    중복되는 부분 함수로 처리
    읽어온 값이 오류로 비어있는 경우 다시 읽어오도록 수정

--- fileflag.py
+++ fileflag.py
@@ -6,9 +6,7 @@ def fileFlag_on(n):  # 0 : memory_temp, 1 : training_model, 2 : model_temp, 3 : 
 
     
     while 1:
-        f = open('fileflag.txt', mode = 'rt', encoding = 'utf-8')
-        flag_training_model = list(f.read(4))
-        f.close()
+        flag_training_model = read_fileflag()
 
         # print(''.join(flag_training_model))
 
@@ -22,9 +20,8 @@ def fileFlag_on(n):  # 0 : memory_temp, 1 : training_model, 2 : model_temp, 3 : 
             time.sleep(sleep_time)
 
     flag_training_model[n] = '1'
-    f = open('fileflag.txt', mode = 'wt', encoding = 'utf-8')
-    f.write(''.join(flag_training_model))
-    f.close()
+
+    write_fileflag(flag_training_model)
 
     print('File flag ' + str(n) + ' ON')
 
@@ -34,24 +31,34 @@ def fileFlag_on(n):  # 0 : memory_temp, 1 : training_model, 2 : model_temp, 3 : 
 # time.sleep(sleep_time)
 
 def fileFlag_off(n):
-
-    f = open('fileflag.txt', mode = 'rt', encoding = 'utf-8')
-    flag_training_model = list(f.read(4))
-    f.close()
+    flag_training_model = read_fileflag()
 
     # print(''.join(flag_training_model))
 
     flag_training_model[n] = '0'
-    f = open('fileflag.txt', mode = 'wt', encoding = 'utf-8')
-    f.write(''.join(flag_training_model))
-    f.close()
+    
+    write_fileflag(flag_training_model)
 
     print('File flag ' + str(n) + ' OFF')
     # print(''.join(flag_training_model))
 
 def get_fileFlag(n):
-    f = open('fileflag.txt', mode = 'rt', encoding = 'utf-8')
-    flag_training_model = list(f.read(4))
-    f.close()
+    flag_training_model = read_fileflag()
 
     return flag_training_model[n]
+
+def read_fileflag():
+    while 1:
+        f = open('fileflag.txt', mode = 'rt', encoding = 'utf-8')
+        flag_training_model = list(f.read(4))
+        f.close()
+
+        if flag_training_model: # 비어있으면 다시 읽어옴
+            break
+
+    return flag_training_model
+
+def write_fileflag(flag_training_model):
+    f = open('fileflag.txt', mode = 'wt', encoding = 'utf-8')
+    f.write(''.join(flag_training_model))
+    f.close()
